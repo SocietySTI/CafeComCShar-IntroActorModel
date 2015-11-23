c05326df653f9f6728d785df740f1892178a9175
Gabriel Schmitt Kohlrausch <gabriel@society.com.br>
2015-11-23 15:17:12 -0200

    projeto base para fazer implementações passo a passo

---
+++ Depois/Encontro-01/IntroActorModel.ExibirFilmes/Atores/ConsoleWriterActor.cs
@@ -0,0 +1,48 @@
+﻿using Akka.Actor;
+using System;
+
+namespace IntroActorModel.ExibirFilmes.Atores
+{
+    public class ConsoleWriterActor : ReceiveActor
+    {
+        #region Mensagens deste Ator
+
+        public abstract class Escrever
+        {
+            public string Mensagem { get; private set; }
+            protected Escrever(string mensagem)
+            {
+                Mensagem = mensagem;
+            }
+        }
+
+        public class EscreverEmVerdeMensagem : Escrever
+        {
+            public EscreverEmVerdeMensagem(string mensagem) : base(mensagem) { }
+        }
+
+        public class EscreverEmVermelho : Escrever
+        {
+            public EscreverEmVermelho(string mensagem) : base(mensagem) { }
+        }
+
+        #endregion
+
+        public ConsoleWriterActor()
+        {
+            Receive<EscreverEmVerdeMensagem>(mensagem => HandleEscreverEmVerde(mensagem)); 
+        }
+
+        private void HandleEscreverEmVerde(EscreverEmVerdeMensagem mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            EscreverMensagem(mensagem.Mensagem);
+        }
+
+        private void EscreverMensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+
+}
---
+++ Depois/Encontro-01/IntroActorModel.ExibirFilmes/Atores/ReprodutorStreamingActor.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroActorModel.ExibirFilmes.Atores
+{
+    public class ReprodutorStreamingActor : ReceiveActor
+    {
+        #region Mensagens deste Ator
+        public class ReproduzirFilmeMensagem
+        {
+            public string TituloFilme { get; private set; }
+            public string CodigoUsuario { get; private set; }
+
+            public ReproduzirFilmeMensagem(string tituloFilme, string codigoUsuario)
+            {
+                TituloFilme = tituloFilme;
+                CodigoUsuario = codigoUsuario;
+            }
+        }
+        #endregion
+
+        private IActorRef _atorParaEscritaEmTela;
+
+        public ReprodutorStreamingActor(IActorRef atorParaEscritaEmTela)
+        {
+            _atorParaEscritaEmTela = atorParaEscritaEmTela;
+            Receive<ReproduzirFilmeMensagem>(mensagem => HandleReproduzirFilme(mensagem));
+        }
+
+        private void HandleReproduzirFilme(ReproduzirFilmeMensagem mensagem)
+        {
+            string mensagemParaExibir = String.Format("Usuário {0} iniciou a reprodução do filme {1}", mensagem.CodigoUsuario, mensagem.TituloFilme);
+
+            _atorParaEscritaEmTela.Tell(new ConsoleWriterActor.EscreverEmVerdeMensagem(mensagemParaExibir));
+        }
+    }
+}
